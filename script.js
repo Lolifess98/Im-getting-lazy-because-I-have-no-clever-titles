46ad5194613cff9efcafe3ffe4d8c377204042e3
Lolifess98 <43540417+Lolifess98@users.noreply.github.com>
2019-03-27 08:28:32 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,27 @@
+$("button").click(function() {
+    var age=$("#ageinput").val();
+    var location=$("#locationnumber").val();
+    var red=$("#colorinput").val();
+    var answer=$("#answer").val();
+
+
+if (age > 18 && "#locationnumber" === 49 && "#colorinput" === "red" && "#answer" === "It didn't happen") {
+   $("p").text("You are Phone guy!");
+
+}
+else if (age > 18 && location === 49 && red && "It didn't happen") {
+
+
+
+} else if  {
+
+
+
+} else if (								) {
+
+
+
+} else {
+
+
+}
